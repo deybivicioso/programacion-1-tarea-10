f9b97b97e066c9c5acf87f741934abecfcb72a47
deybivicioso <darlexstv@gmail.com>
2022-11-30 17:10:06 -0400

    100%

--- src/App.java
+++ src/App.java
@@ -3,11 +3,6 @@ import clase_Principal.ventanaP;
 public class App {
     public static void main(String[] args) throws Exception {
         ventanaP practica = new ventanaP();
-        /*continuar agregando un jseparator;
-         * luego de finil los rangos de edad para cada etapa de vida EJ:+18 adulto;
-         * crear un frame donde se visualice la tabla;
-         * crear los botones de guardar en la tabla, visualizar la tabla, eliminar datos de la tabla;
-         * crear una ventana emergente con los datos que se ingresen en la tabla.
-         */
+        
     }
 }
